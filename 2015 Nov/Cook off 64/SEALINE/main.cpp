6e73ef6ddc617e8644c2106dcdb11b20b4156dc0
Md. Shadekur Rahman <shadekcse07@gmail.com>
2015-11-23 00:40:18 +0600

    using vector insert and algorithm find method of c++

---
+++ 2015 Nov/Cook off 64/SEALINE/main.cpp
@@ -0,0 +1,90 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<list>
+#include <bitset>
+#include<iomanip>
+#include <fstream>
+#include<ctime>
+
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define MX 1000000
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int t;
+    cin >> t;
+    int n;
+    int i,j,k;
+    int a;
+    vector<int>:: iterator it;
+    vector<int> tmp;
+    //tmp.pb(1);
+     //tmp.insert(tmp.begin(),2);
+    //tmp.insert(tmp.begin()+1,3);
+    //cout << tmp[0] << " " << tmp[1]  << endl;
+    while(t--){
+        vector<int> v;
+        v.clear();
+        scanf("%d",&n);
+        int cnt=0;
+        forl(i,0,n){
+            scanf("%d",&a);
+            if(a == 0){
+                v.insert(v.begin(),i+1);
+            }else{
+                it = find(all(v),a);
+                if(it != v.end()){
+                    int ind = it - v.begin();
+                    cnt += min(ind+1, i-(ind+1));
+                    v.insert(v.begin()+(ind+1), i+1);
+                }else{
+                    //cout << "fgh";
+                }
+                
+            }
+        }
+        cout << cnt << endl;
+    }
+    return 0;
+}
