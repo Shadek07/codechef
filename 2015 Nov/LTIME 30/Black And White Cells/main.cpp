53ee4c76f4bbb5bf8d116acb476820d60acd4655
Md. Shadekur Rahman <shadekcse07@gmail.com>
2015-12-06 16:45:19 +0600

    Game theory: Nim

---
+++ 2015 Nov/LTIME 30/Black And White Cells/main.cpp
@@ -0,0 +1,96 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<list>
+#include <bitset>
+#include<iomanip>
+#include <fstream>
+#include<ctime>
+
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+
+bool isZero(int a){
+    return a == 0;
+}
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int t;
+    char in[10005];
+    int i,j,k;
+    cin >> t;
+    getchar();
+    while(t--){
+        gets(in);
+        k = strlen(in);
+        forl(i,0,k){
+            if(in[i] == 'W'){
+                j = i;
+                break;
+            }
+        }
+        k = k - j - 1;
+        //cout << j << " " << k << endl;
+        if(isZero(j) && !isZero(k)){
+            cout << "Aleksa\n";
+            continue; 
+        }
+        if(isZero(k) && !isZero(j)){
+            cout << "Aleksa\n";
+            continue; 
+        }
+        
+        
+        if(j == 1 && k == 1){
+            cout << "Chef\n";
+            continue; 
+        }
+        
+        int a = j+k-2;
+        if( (j^k) ==0){
+           cout << "Chef\n"; 
+        }else{
+           cout << "Aleksa\n";
+        }
+    }
+    return 0;
+}
