cf150a35c85af44ecaa905118ad1dccbc8bc6837
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-02-24 10:02:10 +0600

    adhoc, directed acyclic graph (DAG)

---
+++ 2016 Feb/Long Challenge/Chef-Detective/main.cpp
@@ -0,0 +1,79 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<list>
+#include <bitset>
+#include<iomanip>
+#include <fstream>
+#include<ctime>
+#include<climits>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int vis[MAX];
+    int n;
+    int i,j,k;
+    while(cin >> n){
+        ms(vis,false);
+        forl(i,0,n){
+            cin >> k;
+            vis[k] = true;
+        }
+        bool fir=true;
+        forl(i,1,n+1){
+            if(vis[i] == false){
+                if(!fir){
+                    cout << " ";
+                }else{
+                    fir=false;
+                }
+                cout << i;
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
