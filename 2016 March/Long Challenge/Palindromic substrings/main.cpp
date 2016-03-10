94dbecdf863e0ee54e4c2ef5af17a3d61e0581b1
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-03-11 02:55:09 +0600

    string, palindrome

---
+++ 2016 March/Long Challenge/Palindromic substrings/main.cpp
@@ -0,0 +1,89 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<vector>
+#include<map>
+#include<set>
+#include<string>
+#include<cmath>
+#include<cstdlib>
+#include<cctype>
+#include<algorithm>
+#include<queue>
+#include<sstream>
+#include<stack>
+#include<list>
+#include <bitset>
+#include<iomanip>
+#include <fstream>
+#include<ctime>
+#include<climits>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 1005
+#define INF 9999999
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int t;
+    cin >> t;
+    getchar();
+    bool isPresent[26];
+    char A[MAX];
+    char B[MAX];
+    int i;
+    bool found;
+    while(t--){
+        gets(A);
+        gets(B);
+        int n = strlen(A);
+        int m = strlen(B);
+        found = false;
+        ms(isPresent, false);
+        forl(i,0,n){
+            isPresent[ A[i]-'a' ]=true;
+        }
+        forl(i,0,m){
+            if(isPresent[ B[i]-'a' ] == true){
+                found = true;
+                break;
+            }
+        }
+        if(found){
+            cout << "Yes\n";
+        }
+        else{
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
