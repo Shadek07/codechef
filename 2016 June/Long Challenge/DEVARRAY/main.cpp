4509a35b9adccb85273c45c1bfffe0888e51232e
Md. Shadekur Rahman <shadekcse07@gmail.com>
2016-06-11 08:52:02 +0600

    ad hoc

---
+++ 2016 June/Long Challenge/DEVARRAY/main.cpp
@@ -0,0 +1,58 @@
+
+#pragma comment(linker,"/STACK:16777216")
+#pragma  warning ( disable: 4786)
+#include <bits/stdc++.h>
+using namespace std;
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+#define forl(i,a,b) for ( i = a; i < b; i++)
+#define fore(i,a,b) for ( i = a; i <= b; i++)
+#define forrev(i,a,b) for ( i = a; i >= b; i--)
+#define MID(a, b) (a + ((b - a) >> 1))
+#define pb push_back
+typedef long long  LL;
+#define in(a,b,c) ((a) <= (b) && (b) <= (c))
+#define ms(a,b) memset((a),(b),sizeof(a))
+
+#define all(v) (v).begin(),(v).end()
+#define pb push_back
+#define ull unsigned long long int
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+
+string to_string(long long x) { stringstream ss; ss << x; return ss.str(); }
+long long to_int(const string &s) { stringstream ss; ss << s; long long x; ss >> x; return x; }
+
+#define MAX 100005
+#define INF 9999999
+
+int main(void)
+{
+    //freopen("1.txt", "r", stdin);
+    //freopen("2.txt", "w", stdout);
+    int n,t,i;
+    int mn,mx,a;
+    while(cin >> n >> t){
+         mn = 1e9;
+         mx = 0;
+        forl(i,0,n){
+            cin >> a;
+            mn = min(mn,a);
+            mx = max(mx,a);
+        }
+        forl(i,0,t){
+            cin >> a;
+            if(a>=mn && a<=mx){
+                cout << "Yes\n";
+            }else{
+                cout << "No\n";
+            }
+        }
+    }
+    return 0;
+}
+
